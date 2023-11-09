bb9162188dc33281376f534d59f34ac6327cd16c
levisattonpu <levi.satto@npu.com.br>
2023-11-09 09:15:05 -0300

    fix: try-except

--- services/worker.py
+++ services/worker.py
@@ -8,15 +8,19 @@ load_dotenv()
 
 
 def get_weather(querystring:str):
-    url = "https://weatherapi-com.p.rapidapi.com/current.json"
-    headers = {
-        "X-RapidAPI-Key": os.getenv('RAPIDKEY'),
-        "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com"
-    }
+    try:
+        url = "https://weatherapi-com.p.rapidapi.com/current.json"
+        headers = {
+            "X-RapidAPI-Key": os.getenv('RAPIDKEY'),
+            "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com"
+        }
 
-    response = requests.get(url, headers=headers, params=querystring)
-    data = response.json()
-    local = {"Local": data['location']['name'], "Temperatura":data['current']['temp_c'], 
-             "Condições":data['current']['condition']['text']}
-    weather_forecast_json = json.dumps(local)
-    return weather_forecast_json
+        response = requests.get(url, headers=headers, params=querystring)
+        data = response.json()
+        print(data)
+        local = {"Local": data['location']['name'], "Temperatura":data['current']['temp_c'], 
+                "Condições":data['current']['condition']['text']}
+        weather_forecast_json = json.dumps(local)
+        return weather_forecast_json
+    except Exception as e:
+        raise e
